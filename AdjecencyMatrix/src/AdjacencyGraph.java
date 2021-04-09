6f2fee0b7658504b4a26a9fd6430eaedb44cf8f1
Max de Visser <m.madv96@gmail.com>
2021-04-09 16:11:28 +0200

    Første version der virker
    Version der virker med PQ men ikke med minHeap

--- AdjecencyMatrix/src/AdjacencyGraph.java
+++ AdjecencyMatrix/src/AdjacencyGraph.java
@@ -1,147 +1,182 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
-
-// se link: https://www.youtube.com/watch?v=jsmMtJpPnhU
-
-//------------------------------ADJACENCY GRAPH----------------------------------------//
+//---------------------Adjacency Graph----------------------
 
 public class AdjacencyGraph {
-    //---ATTRIBUTES
-    ArrayList<Vertex> vertices;
+    ArrayList<Node> nodes; //the list used for making the adjacency graph
 
-    //---METHODS
-    public AdjacencyGraph() {
-        vertices = new ArrayList<Vertex>(); //creates a list for vertices in the graph
+    public AdjacencyGraph() { //constructor
+        nodes = new ArrayList<>();
     }
 
-    public void addVertex(Vertex v) { //ads a vertex to the vertices list
-        vertices.add(v);
+    public void addNode(Node n) { //adds a node to the adjacency graph
+        nodes.add(n);
     }
 
-    public void addEdge(Vertex one, Vertex two, Integer w) { //ADDS AN ADGE TO THE GRAPH
-        if (!(vertices.contains(one) && vertices.contains(two))) { //test whether the vertex exists in the graph
-            System.out.println(" Vertex not in graph");
+    public void addEdge(Node nodeOne, Node nodeTwo, Integer edgeWeight) { //adds an edge to the adjacency graph
+        if (!(nodes.contains(nodeOne) && nodes.contains(nodeTwo))) {
+            System.out.println("Node doesn't exist in the graph");
             return;
         }
-        Edge e1 = new Edge(one, two, w);
-        Edge e2 = new Edge(two, one, w); //ADDED FOR UNDIRECTING THE GRAPH
+        //there are two edges created to represent, that the graph is bidirectional (aka undirectional)
+        Edge edge1 = new Edge(nodeOne, nodeTwo, edgeWeight);
+        Edge edge2 = new Edge(nodeTwo, nodeOne, edgeWeight);
     }
 
-    //---PRIMS ALGORITHM FOR FINDING MST
-    public void primsMST(){
-        int[] distance = new int[vertices.size()];
-        int[] predecessor = new int[vertices.size()];
-        boolean[] visited = new boolean[vertices.size()]; //list to keep track of vertices we have visited
 
-        MinHeap<Pair> Q = new MinHeap<>();
-        PriorityQueue<Pair> PQ = new PriorityQueue<>();
-
-        ArrayList<Pair> VertexPairs = new ArrayList<>();
-
-        Arrays.fill(distance, Integer.MAX_VALUE); //Integer.MAX_VALUE is supposed to resemble infinity
-        Arrays.fill(predecessor,-1); //fill fylder listen med en værdi
-        Arrays.fill(visited, false);
-
-        if(vertices.size() > 0){
-            distance[0] = 0;
-        }
-
-        for (int i = 0; i < vertices.size(); i++) {
-            VertexPairs.add(new Pair(distance[i], i));
-            Q.Insert(VertexPairs.get(i));
-        }
-
-        int MST = 0;
-
-        while (!Q.isEmpty()){
-            Pair u = Q.extractMin();
-
-            for (int v = 0; v < vertices.size(); v++) {
-                //WATCH THE VIDEO WHERE THE PRIMS ALGORITHM IS IMPLEMENTED to understand what to do here
+    public void printGraph(){
+        for (int i = 0; i < nodes.size(); i++) {
+            Node currentNode = nodes.get(i);
+            System.out.println("Connections from " + currentNode.getNodeName() + " are:");
+            for (int j = 0; j < currentNode.outgoingEdgesFromNode.size(); j++) {
+                Edge currentEdge = currentNode.outgoingEdgesFromNode.get(j);
+                System.out.println(currentNode.outgoingEdgesFromNode.get(j).getToNote().getNodeName() + " - distance: " + currentEdge.getWeight() + " km");
             }
+            System.out.println();
         }
-
     }
-    public void PrintGraph() {
-        for (int i = 0; i < vertices.size(); i++) {
-            System.out.println(" From the Vertex: " + vertices.get(i).vertexName);
-            Vertex currentFrom = vertices.get(i); //states which vertex we are on
 
-            for (int j = 0; j < currentFrom.OutEdges.size(); j++) {
-                Edge currentEdge = currentFrom.OutEdges.get(j);
-                System.out.println(" To: " + currentEdge.two.vertexName + " weight: " + currentEdge.weight);
-            }
-            System.out.println(" ");
+    int MSTminheap = 0; //Brugt kun til prismUsingHeap
+    public void primsUsingMinHeap(){
+        MinHeap<Node> citiesMinHeap = new MinHeap<Node>();
+
+        if (nodes.size() > 0) {
+            nodes.get(0).distance = 0;
+            citiesMinHeap.Insert(nodes.get(0));
         }
-    } //doesnt work properly. It prints as if the graph is directional, which it is not
+
+        while(!citiesMinHeap.isEmpty()){
+            Node currentNode = citiesMinHeap.extractMin();
+
+            if(!currentNode.hasBeenVisited){
+                for (int i = 0; i < currentNode.getOutgoingEdgesFromNode().size(); i++) {
+
+                    Node currentToNode = currentNode.getOutgoingEdgesFromNode().get(i).getToNote();
+                    Integer currentEdgeWeight = currentNode.getOutgoingEdgesFromNode().get(i).getWeight();
+
+                    if((!currentToNode.hasBeenVisited) && currentEdgeWeight < currentToNode.distance){
+                        currentToNode.distance = currentEdgeWeight;
+                        currentToNode.predecessorNode = currentNode;
+                        citiesMinHeap.Insert(currentToNode);
+                    }
+                }
+
+                currentNode.hasBeenVisited = true;
+                MSTminheap += currentNode.distance; //sum af MST
+            }
+        }
+    }
+
+    int MST = 0; //Brugt kun til primsUsingPQ
+    public void primsUsingPQ(){
+        PriorityQueue <Node> citiesPQ = new PriorityQueue<Node>();
+
+        if (nodes.size() > 0) {
+            nodes.get(0).distance = 0;
+            citiesPQ.offer(nodes.get(0));
+        }
+
+        while(!citiesPQ.isEmpty()){
+            Node currentNode = citiesPQ.poll();
+
+            if(!currentNode.hasBeenVisited){
+                for (int i = 0; i < currentNode.getOutgoingEdgesFromNode().size(); i++) {
+
+                    Node toNode = currentNode.getOutgoingEdgesFromNode().get(i).getToNote();
+                    Integer currentEdgeWeight = currentNode.getOutgoingEdgesFromNode().get(i).getWeight();
+
+                    if((!toNode.hasBeenVisited) && currentEdgeWeight < toNode.distance){
+                        toNode.distance = currentEdgeWeight;
+                        toNode.predecessorNode = currentNode;
+                        citiesPQ.offer(toNode);
+                    }
+                }
+
+                currentNode.hasBeenVisited = true;
+                MST += currentNode.distance; //sum af MST
+            }
+        }
+    }
+
+
+    public void printMST(){
+        System.out.println("The Minimum Spanning Tree is connected as such: \n");
+        for (int i = 0; i < nodes.size(); i++) {
+            Node predecessorNode = nodes.get(i).predecessorNode;
+
+            if (predecessorNode != null) { //since all Nodes initially dont have a predecessor node initalized, we can sort the ones that has
+                //been givin a predecessor in Prims algorithm (an so, we will know that it is part of the MST).
+                System.out.println(predecessorNode.getNodeName() +
+                        " to " + nodes.get(i).getNodeName() + " Edge Weight: " +
+                        nodes.get(i).distance + "km");
+            }
+        }
+        System.out.println("\nTotal distance covered by the MST: " + MST + " km");
+        System.out.println("Total Cost of the grid: " + MST*100000 + " kr");
+    }
 }
 
-//---------------------------------VERTEX-------------------------------------//
+//------------------------NODE-------------------------------
+class Node  implements Comparable<Node>{
+    //---ATTRIBUTES---
+    String nodeName;
+    ArrayList<Edge> outgoingEdgesFromNode; //list of edges going out from the vertex
+    Integer distance = Integer.MAX_VALUE; //Variabel representing the distance to the node which is initialized as infinity except the choosen starting node (which is 0)
+    Node predecessorNode = null; //used to store the previous Node to the current one, when traversing through the tree in Prims algorithm
+    Boolean hasBeenVisited = false;
 
-class Vertex implements Comparable<Vertex> {
-    //---ATTRIBUTES:
-    String vertexName;
-    ArrayList<Edge> OutEdges; //list of edges going out from the vertex
-    Integer dist = Integer.MAX_VALUE; //resembles infinity in this context
-
-
-    //---METHODS:
-    public Vertex(String name) { //constructor for the vertex
-        vertexName = name;
-        OutEdges = new ArrayList<Edge>(); //list of edges going out from this vertex
+    //---METHODS---
+    public Node(String name) { //constructor for the vertex
+        nodeName = name;
+        outgoingEdgesFromNode = new ArrayList<>(); //list of edges going out from this vertex
     }
 
-    public void addOutEdge(Edge e) { //adds a edge to the list of edges going out from this Vertex
-        OutEdges.add(e);
+    public void addOutgoingEdge(Edge e) { //adds a edge to the list of edges going out from this Vertex
+        outgoingEdgesFromNode.add(e);
     }
 
     @Override
-    public int compareTo(Vertex o) {
-        if (this.dist < o.dist)
-        return -1;
-        if (this.dist > o.dist)
-            return 1;
-        return 0;
+    public int compareTo(Node o) { //utilityfunction used to compare distances between nodes
+        //how the function is used: firstNode.compareTo(secondNode)
+        if (this.distance < o.distance)
+            return -1; //resembels that the distance from firstNode is SMALLER than distance from secondNote
+        if (this.distance > o.distance)
+            return 1; //resembels that the distance from firstNode is LARGER than distance from secondNote
+        return 0; //resembles if they are of equal value
+    }
+
+
+
+    //---GETTERS AND SETTERS
+    public String getNodeName() {
+        return nodeName;
+    }
+    public ArrayList<Edge> getOutgoingEdgesFromNode() {
+        return outgoingEdgesFromNode;
     }
 }
 
-//--------------------------------EDGE--------------------------------------//
+//------------------------EDGE-------------------------------
+class Edge{
+    //---ATTRIBUTES---
+    Node fromNote;
+    Node toNote;
+    Integer weight; //represents the distance between cities
 
-class Edge {
-    //---ATTRIBUTES
-    Integer weight;
-    Vertex one; //represents the two vertexes the edge connects
-    Vertex two;
-
-    //---METHODS
-    public Edge(Vertex first, Vertex second, Integer cost) { //constructor for the edge
-        this.one = first;
-        this.two = second;
-        this.weight = cost;
-        this.one.addOutEdge(this); //maybe delete this
-    }
-}
-
-//-------------------------------PAIR---------------------------------------//
-
-//THE PAIR CLASS WAS COPIED FROM THE MATRIX GRAPH IMPLEMENTATION OF PRIMS ALGORITHM
-//im not sure if it needs to be used yet
-class Pair implements Comparable<Pair>{
-    //---ATTRIBUTES
-    Integer distance; //distance between two vertices
-    Integer index;
-
-
-    //---METHODS
-    public Pair(Integer distance, Integer index){ //constructor for a Pair
-        this.distance=distance;
-        this.index = index;
+    //---METHODS---
+    public Edge (Node from, Node to, Integer w){ //constructor
+        this.fromNote = from;
+        this.toNote = to;
+        this.weight = w;
+        this.fromNote.addOutgoingEdge(this); //adds the current edge created to the fromNode's outgoindEdgeList
     }
 
-    @Override
-    public int compareTo(Pair p){ //return the distance between to vertices
-        return this.distance.compareTo(p.distance);
+    //---GETTERS AND SETTERS
+    public Node getToNote() {
+        return toNote;
+    }
+    public Integer getWeight() {
+        return weight;
     }
 }
