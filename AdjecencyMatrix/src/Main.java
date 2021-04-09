7046a10332df568669539be756210edce26f771c
Max de Visser <m.madv96@gmail.com>
2021-04-09 16:11:48 +0200

    Update Main.java

--- AdjecencyMatrix/src/Main.java
+++ AdjecencyMatrix/src/Main.java
@@ -1,54 +1,56 @@
-import java.util.jar.JarEntry; //bliver ikke brugt endnu
-
 public class Main {
 
     public static void main(String[] args) {
-        AdjacencyGraph Cities = new AdjacencyGraph(); //initializes the adjacency graph
+        AdjacencyGraph cities = new AdjacencyGraph(); //initializes the adjacency graph holding all the cities
 
-        Vertex Eskildstrup = new Vertex("Eskildsstrup");   Cities.addVertex(Eskildstrup);
-        Vertex Haslev = new Vertex("Haslev");              Cities.addVertex(Haslev);
-        Vertex Holbæk = new Vertex( "Holbæk");             Cities.addVertex(Holbæk);
-        Vertex Jærgerspris = new Vertex("Jærgerspris");    Cities.addVertex(Jærgerspris);
-        Vertex Kalundborg = new Vertex("Kalundborg");      Cities.addVertex(Kalundborg);
-        Vertex Korsør = new Vertex( "Korsør");             Cities.addVertex(Korsør);
-        Vertex Køge = new Vertex("Køge");                  Cities.addVertex(Køge);
-        Vertex Maribo = new Vertex("Maribo");              Cities.addVertex(Maribo);
-        Vertex Næstved = new Vertex("Næstved");            Cities.addVertex(Næstved);
-        Vertex Ringsted = new Vertex("Ringsted");          Cities.addVertex(Ringsted);
-        Vertex Slagelse = new Vertex("Slagelse");          Cities.addVertex(Slagelse);
-        Vertex Nykøbing_F = new Vertex("Nykøbing F");      Cities.addVertex(Nykøbing_F);
-        Vertex Vordingborg = new Vertex("Vordingborg");    Cities.addVertex(Vordingborg);
-        Vertex Roskilde = new Vertex("Roskilde");          Cities.addVertex(Roskilde);
-        Vertex Sorø = new Vertex("Sorø");                  Cities.addVertex(Sorø);
-        Vertex Nakskov = new Vertex("Nakskov");            Cities.addVertex(Nakskov);
+        //---initializing Nodes and adding them to the adjacency graph
+        Node Eskildstrup = new Node("Eskildsstrup");            cities.addNode(Eskildstrup);
+        Node Haslev = new Node("Haslev");                       cities.addNode(Haslev);
+        Node Holbæk = new Node( "Holbæk");                      cities.addNode(Holbæk);
+        Node Jærgerspris = new Node("Jærgerspris");             cities.addNode(Jærgerspris);
+        Node Kalundborg = new Node("Kalundborg");               cities.addNode(Kalundborg);
+        Node Korsør = new Node( "Korsør");                      cities.addNode(Korsør);
+        Node Køge = new Node("Køge");                           cities.addNode(Køge);
+        Node Maribo = new Node("Maribo");                       cities.addNode(Maribo);
+        Node Næstved = new Node("Næstved");                     cities.addNode(Næstved);
+        Node Ringsted = new Node("Ringsted");                   cities.addNode(Ringsted);
+        Node Slagelse = new Node("Slagelse");                   cities.addNode(Slagelse);
+        Node Nykøbing_F = new Node("Nykøbing F");               cities.addNode(Nykøbing_F);
+        Node Vordingborg = new Node("Vordingborg");             cities.addNode(Vordingborg);
+        Node Roskilde = new Node("Roskilde");                   cities.addNode(Roskilde);
+        Node Sorø = new Node("Sorø");                           cities.addNode(Sorø);
+        Node Nakskov = new Node("Nakskov");                     cities.addNode(Nakskov);
 
-        Cities.addEdge(Eskildstrup,Maribo,28);             Cities.addEdge(Jærgerspris,Sorø, 51);
-        Cities.addEdge(Eskildstrup,Nykøbing_F, 13);        Cities.addEdge(Kalundborg,Ringsted, 62);
-        Cities.addEdge(Eskildstrup,Vordingborg,24);        Cities.addEdge(Kalundborg,Roskilde, 70);
-        Cities.addEdge(Haslev,Korsør,60);                  Cities.addEdge(Kalundborg,Slagelse, 39);
-        Cities.addEdge(Haslev,Køge,24);                    Cities.addEdge(Kalundborg,Sorø, 51);
-        Cities.addEdge(Haslev,Næstved,25);                 Cities.addEdge(Korsør,Næstved, 45);
-        Cities.addEdge(Haslev,Ringsted,19);                Cities.addEdge(Korsør,Slagelse, 20);
-        Cities.addEdge(Haslev,Roskilde, 47);               Cities.addEdge(Køge,Næstved, 45);
-        Cities.addEdge(Haslev,Slagelse, 48);               Cities.addEdge(Køge,Ringsted, 28);
-        Cities.addEdge(Haslev,Sorø,34);                    Cities.addEdge(Køge,Roskilde, 25);
-        Cities.addEdge(Haslev,Vordingborg, 40);            Cities.addEdge(Køge,Vordingborg, 60);
-        Cities.addEdge(Holbæk,Jærgerspris,34);             Cities.addEdge(Maribo,Nakskov, 27);
-        Cities.addEdge(Holbæk,Kalundborg, 44);             Cities.addEdge(Maribo,Nykøbing_F, 26);
-        Cities.addEdge(Holbæk,Korsør, 66);                 Cities.addEdge(Næstved,Roskilde, 57);
-        Cities.addEdge(Holbæk,Ringsted, 36);               Cities.addEdge(Næstved,Ringsted, 26);
-        Cities.addEdge(Holbæk,Roskilde, 32);               Cities.addEdge(Næstved,Slagelse, 37);
-        Cities.addEdge(Holbæk,Slagelse, 46);               Cities.addEdge(Næstved,Sorø, 32);
-        Cities.addEdge(Holbæk,Sorø, 63);                   Cities.addEdge(Næstved,Vordingborg, 28);
-        Cities.addEdge(Jærgerspris,Korsør, 95);            Cities.addEdge(Ringsted,Roskilde, 31);
-        Cities.addEdge(Jærgerspris,Køge, 58);              Cities.addEdge(Ringsted,Sorø, 15);
-        Cities.addEdge(Jærgerspris,Ringsted, 56);          Cities.addEdge(Ringsted,Vordingborg, 58);
-        Cities.addEdge(Jærgerspris,Roskilde, 33);          Cities.addEdge(Slagelse,Sorø, 14);
-        Cities.addEdge(Jærgerspris,Slagelse, 74);
+        //---Adding all the connections (edges) between cities
+        cities.addEdge(Eskildstrup,Maribo,28);             cities.addEdge(Jærgerspris,Sorø, 51);
+        cities.addEdge(Eskildstrup,Nykøbing_F, 13);        cities.addEdge(Kalundborg,Ringsted, 62);
+        cities.addEdge(Eskildstrup,Vordingborg,24);        cities.addEdge(Kalundborg,Roskilde, 70);
+        cities.addEdge(Haslev,Korsør,60);                  cities.addEdge(Kalundborg,Slagelse, 39);
+        cities.addEdge(Haslev,Køge,24);                    cities.addEdge(Kalundborg,Sorø, 51);
+        cities.addEdge(Haslev,Næstved,25);                 cities.addEdge(Korsør,Næstved, 45);
+        cities.addEdge(Haslev,Ringsted,19);                cities.addEdge(Korsør,Slagelse, 20);
+        cities.addEdge(Haslev,Roskilde, 47);               cities.addEdge(Køge,Næstved, 45);
+        cities.addEdge(Haslev,Slagelse, 48);               cities.addEdge(Køge,Ringsted, 28);
+        cities.addEdge(Haslev,Sorø,34);                    cities.addEdge(Køge,Roskilde, 25);
+        cities.addEdge(Haslev,Vordingborg, 40);            cities.addEdge(Køge,Vordingborg, 60);
+        cities.addEdge(Holbæk,Jærgerspris,34);             cities.addEdge(Maribo,Nakskov, 27);
+        cities.addEdge(Holbæk,Kalundborg, 44);             cities.addEdge(Maribo,Nykøbing_F, 26);
+        cities.addEdge(Holbæk,Korsør, 66);                 cities.addEdge(Næstved,Roskilde, 57);
+        cities.addEdge(Holbæk,Ringsted, 36);               cities.addEdge(Næstved,Ringsted, 26);
+        cities.addEdge(Holbæk,Roskilde, 32);               cities.addEdge(Næstved,Slagelse, 37);
+        cities.addEdge(Holbæk,Slagelse, 46);               cities.addEdge(Næstved,Sorø, 32);
+        cities.addEdge(Holbæk,Sorø, 63);                   cities.addEdge(Næstved,Vordingborg, 28);
+        cities.addEdge(Jærgerspris,Korsør, 95);            cities.addEdge(Ringsted,Roskilde, 31);
+        cities.addEdge(Jærgerspris,Køge, 58);              cities.addEdge(Ringsted,Sorø, 15);
+        cities.addEdge(Jærgerspris,Ringsted, 56);          cities.addEdge(Ringsted,Vordingborg, 58);
+        cities.addEdge(Jærgerspris,Roskilde, 33);          cities.addEdge(Slagelse,Sorø, 14);
+        cities.addEdge(Jærgerspris,Slagelse, 74);
 
         //------------------------------OPERATIONS-----------------------------//
 
-        Cities.PrintGraph();
-
+        cities.primsUsingPQ();
+        //cities.primsUsingMinHeap();
+        cities.printMST();
+        //cities.printGraph();
     }
 }
